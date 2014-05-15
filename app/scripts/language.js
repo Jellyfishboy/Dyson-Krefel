58a1a628cac7f57eb6832ff1bf4fc601c797de17
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-05-16 00:01:09 +0100

    Abstracted language data to it's own file.

--- app/scripts/language.js
+++ app/scripts/language.js
@@ -1,33 +1,7 @@
 'use strict';
 
-angular.module('dysonKrefelWebApp', ['pascalprecht.translate', 'ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainController'
-            })
-            .when('/cylinders', {
-                templateUrl: 'views/cylinders.html',
-                controller: 'CylinderController'
-            })
-            .when('/cordless', {
-                templateUrl: 'views/cordless.html',
-                controller: 'CordlessController'
-            })
-            .when('/handhelds', {
-                templateUrl: 'views/handhelds.html',
-                controller: 'HandheldController'
-            })
-            .when('/fans-and-heaters', {
-                templateUrl: 'views/fans.html',
-                controller: 'FanController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
-    .config(['$translateProvider', function($translateProvider) {
+angular.module('dysonKrefelWebApp')
+    .config(['$translateProvider', function ($translateProvider) {
 
         $translateProvider
             .translations('en-GB', {
